03859abe95187e7ecec884f61163239b8f95e247
MarioAlabi <marioedgardovilledaalabi@gmail.com>
2026-02-26 15:02:08 -0600

    otros

--- FrontEnd/src/lib/auth-client.js
+++ FrontEnd/src/lib/auth-client.js
@@ -1,8 +1,7 @@
-// src/lib/auth-client.js
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
-// Forzamos la URL de tu servidor real
+// Forzamos la URL del servidor
 const API_BASE = "https://consultorioback.marioalabi.com"; 
 
 export const authClient = createAuthClient({
