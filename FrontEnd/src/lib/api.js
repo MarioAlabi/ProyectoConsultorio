4194b9aa70287fb226b084a0ee9b372fef79601f
Mario Alabi <mario.villeda@catolica.edu.sv>
2026-02-26 17:44:33 -0600

    cambio localhost

--- FrontEnd/src/lib/api.js
+++ FrontEnd/src/lib/api.js
@@ -3,8 +3,10 @@ import axios from "axios";
 //const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3000";
 const API_BASE_URL = import.meta.env.VITE_API_URL || "https://consultorioback.marioalabi.com";
 
+const API_BASE = import.meta.env.VITE_API_URL || "https://consultorioback.marioalabi.com";
+
 export const api = axios.create({
-    baseURL: `${API_BASE_URL}/api`,
+    baseURL: `${API_BASE}/api`,
     withCredentials: true,
     headers: { "Content-Type": "application/json" },
 });
